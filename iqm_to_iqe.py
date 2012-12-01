f6a46e004fae8d50160e6d48223a59f4659ad3c6
unknown <tor@ccxvii.net>
2012-12-01 17:11:02 +0100

    Convert iqm_to_iqe to python3.

--- iqm_to_iqe.py
+++ iqm_to_iqe.py
@@ -38,9 +38,9 @@ iqm_va_format = {
 
 def cstr(text, ofs):
 	len = 0
-	while text[ofs+len] != "\0":
+	while text[ofs+len] != 0:
 		len += 1
-	return text[ofs:ofs+len]
+	return text[ofs:ofs+len].decode("utf-8", "ignore")
 
 def optscale(scale):
 	x, y, z = scale
@@ -59,17 +59,17 @@ def dump_joints(file, text, num_joints, ofs_joints):
 	for x in range(num_joints):
 		joint = struct.unpack("<Ii10f", file.read(12*4))
 		jointlist += (joint,)
-	print
+	print()
 	for joint in jointlist:
 		name = cstr(text, joint[0])
 		parent = joint[1]
-		print "joint", name, parent
-	print
+		print("joint", name, parent)
+	print()
 	for joint in jointlist:
 		pos = joint[2:5]
 		rot = joint[5:9]
 		scale = joint[9:12]
-		print "pq", fmtp(pos + rot + optscale(scale))
+		print("pq", fmtp(pos + rot + optscale(scale)))
 
 def load_poses(file, num_poses, ofs_poses):
 	file.seek(ofs_poses)
@@ -103,7 +103,7 @@ def dump_frame(poselist, frame):
 			pos = data[0:3]
 			rot = [x for x in data[3:7]]
 			scale = [x for x in optscale(data[7:10])]
-		print "pq", fmtp(pos + rot + scale)
+		print("pq", fmtp(pos + rot + scale))
 
 def dump_anims(file, text, num_anims, ofs_anims, poses, frames):
 	file.seek(ofs_anims)
@@ -112,18 +112,18 @@ def dump_anims(file, text, num_anims, ofs_anims, poses, frames):
 		name = cstr(text, anim[0])
 		first = anim[1]
 		count = anim[2]
-		print
-		print "animation", name
-		print "framerate %g" % anim[3]
-		if anim[4]: print "loop"
+		print()
+		print("animation", name)
+		print("framerate %g" % anim[3])
+		if anim[4]: print("loop")
 		for y in range(first, first+count):
-			print
-			print "frame"
+			print()
+			print("frame")
 			dump_frame(poses, frames[y])
 
 def load_array(file, format, size, offset, count):
 	if format != 1 and format != 7:
-		print >>sys.stderr, "can only handle ubyte and float arrays"
+		print("can only handle ubyte and float arrays", file=sys.stderr)
 		sys.exit(1)
 	if format == 1: A="<"+"B"*size; S=1*size
 	if format == 7: A="<"+"f"*size; S=4*size
@@ -136,7 +136,7 @@ def load_array(file, format, size, offset, count):
 
 def load_verts(file, text, num_vertexarrays, num_vertexes, ofs_vertexarrays):
 	file.seek(ofs_vertexarrays)
-	print
+	print()
 	custom = 16
 	valist = []
 	for x in range(num_vertexarrays):
@@ -151,8 +151,8 @@ def load_verts(file, text, num_vertexarrays, num_vertexes, ofs_vertexarrays):
 			type = custom
 			custom = custom + 1
 		verts[type] = load_array(file, format, size, offset, num_vertexes)
-		if type >= 16: print "vertexarray", iqm_va_type[type], iqm_va_format[format], size, type_name
-		else: print "vertexarray", iqm_va_type[type], iqm_va_format[format], size
+		if type >= 16: print("vertexarray", iqm_va_type[type], iqm_va_format[format], size, type_name)
+		else: print("vertexarray", iqm_va_type[type], iqm_va_format[format], size)
 	return verts
 
 def load_tris(file, num_triangles, ofs_triangles, ofs_adjacency):
@@ -165,26 +165,26 @@ def load_tris(file, num_triangles, ofs_triangles, ofs_adjacency):
 
 def dump_verts(verts, first, count):
 	for x in range(first, first+count):
-		if verts[0]: print "vp", fmtv(verts[0][x])
-		if verts[1]: print "vt", fmtv(verts[1][x])
-		if verts[2]: print "vn", fmtv(verts[2][x])
-		if verts[3]: print "vx", fmtv(verts[3][x])
+		if verts[0]: print("vp", fmtv(verts[0][x]))
+		if verts[1]: print("vt", fmtv(verts[1][x]))
+		if verts[2]: print("vn", fmtv(verts[2][x]))
+		if verts[3]: print("vx", fmtv(verts[3][x]))
 		if verts[4] and verts[5]:
 			out = "vb"
 			for y in range(4):
 				if verts[5][x][y] > 0:
 					out += " %d" % verts[4][x][y]
 					out += " %.9g" % (verts[5][x][y]/255.0)
-			print out
+			print(out)
 		if verts[6]:
-			print "vc", fmtb(verts[6][x])
+			print("vc", fmtb(verts[6][x]))
 		for i in range(16, 16+10):
-			if verts[i]: print "v%d" % (i-16), fmtv(verts[i][x])
+			if verts[i]: print("v%d" % (i-16), fmtv(verts[i][x]))
 
 def dump_tris(tris, first, count, fv):
 	for x in range(first, first+count):
 		tri = tris[x]
-		print "fm", tri[0]-fv, tri[1]-fv, tri[2]-fv
+		print("fm", tri[0]-fv, tri[1]-fv, tri[2]-fv)
 
 def dump_meshes(file, text, num_meshes, ofs_meshes, verts, tris):
 	file.seek(ofs_meshes)
@@ -193,9 +193,9 @@ def dump_meshes(file, text, num_meshes, ofs_meshes, verts, tris):
 		name = cstr(text, mesh[0])
 		material = cstr(text, mesh[1])
 		v1, vnum, t1, tnum = mesh[2:]
-		print
-		print "mesh", name
-		print "material", material
+		print()
+		print("mesh", name)
+		print("material", material)
 		dump_verts(verts, v1, vnum)
 		dump_tris(tris, t1, tnum, v1)
 
@@ -213,25 +213,15 @@ def dump_iqm(file):
 		num_comment, ofs_comment,
 		num_extensions, ofs_extensions ) = hdr
 
-	if magic != "INTERQUAKEMODEL\0":
-		print >>sys.stderr, "Not an IQM file."
+	if magic != b"INTERQUAKEMODEL\0":
+		print("Not an IQM file (%s)", repr(magic), file=sys.stderr)
 		sys.exit(1)
 
 	if version != 2:
-		print >>sys.stderr, "Not an IQMv2 file."
+		print("Not an IQMv2 file.", file=sys.stderr)
 		sys.exit(1)
 
-	print "# Inter-Quake Export (v%d)" % version
-
-	print >>sys.stderr, "# %d meshes" % num_meshes
-	print >>sys.stderr, "# %d vertex arrays" % num_vertexarrays
-	print >>sys.stderr, "# %d vertices" % num_vertexes
-	print >>sys.stderr, "# %d triangles (adjacency at %d)" % (num_triangles, ofs_adjacency)
-	print >>sys.stderr, "# %d joints" % num_joints
-	print >>sys.stderr, "# %d poses" % num_poses
-	print >>sys.stderr, "# %d anims" % num_anims
-	print >>sys.stderr, "# %d frames (bounds at %d)" % (num_frames, ofs_bounds)
-	print >>sys.stderr, "# %d frame channels" % num_framechannels
+	print("# Inter-Quake Export")
 
 	file.seek(ofs_text)
 	text = file.read(num_text);
