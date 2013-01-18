a2be3ba968c23278ee9a596d11b62bc915f8c25e
Tor Andersson <tor@ccxvii.net>
2013-01-18 23:11:52 +0100

    Clean up iqe script and radial normal calculation.

--- scripts/iqe.py
+++ scripts/iqe.py
@@ -9,6 +9,20 @@
 
 import sys, shlex, fnmatch, os.path, math
 
+def normalize(x, y, z):
+	d = math.sqrt(x*x + y*y + z*z)
+	if d >= 0.001:
+		return x/d, y/d, z/d
+	return 0, 0, 1
+
+def x_to_y(v):
+	x, y, z = v
+	return -y, x, z
+
+def y_to_z(v):
+	x, y, z = v
+	return x, -z, y
+
 class Mesh:
 	def __init__(self, name):
 		self.name = name
@@ -104,10 +118,9 @@ class Model:
 			for line in self.comment:
 				print >>file, line,
 
-def blend_pairs(t):
-	return tuple(zip(t[::2], t[1::2]))
-
 def load_model(file):
+	def blend_pairs(t):
+		return tuple(zip(t[::2], t[1::2]))
 	model = Model()
 	mesh = None
 	pose = model.bindpose
@@ -167,122 +180,10 @@ def load_model(file):
 			comment = True
 	return model
 
-def save_as_obj(model, filename):
-	mtlname = os.path.splitext(filename)[0] + ".mtl"
-	mfile = open(mtlname, "w")
-	print >>mfile, "# Wavefront Object Material"
+# Create backfacing copies of doublesided meshes.
 
-	file = open(filename, "w")
-	print >>file, "# Wavefront Object Model"
-	print >>file, "mtllib", mtlname
-
-	curmesh = None
-	ofs = 1
-	for mesh in model.meshes:
-		if curmesh != mesh.name:
-			print >>file, "g", mesh.name
-		curmesh = mesh.name
-		print >>mfile, "newmtl", "+".join(mesh.material)
-		print >>mfile, "map_Kd", mesh.material[-1] + ".png"
-		print >>file, "usemtl", "+".join(mesh.material)
-		for i in range(len(mesh.positions)):
-			x,y,z = mesh.positions[i]
-			print >>file, "v %.9g %.9g %.9g" % (x,z,-y)
-			x,y,z = mesh.normals[i]
-			print >>file, "vn %.9g %.9g %.9g" % (x,z,-y)
-			x,y = mesh.texcoords[i]
-			print >>file, "vt %.9g %.9g" % (x,1-y)
-		for face in mesh.faces:
-			if len(face) == 3:
-				a,b,c = face[0]+ofs, face[1]+ofs, face[2]+ofs
-				print >>file, "f %d/%d/%d %d/%d/%d %d/%d/%d" % (c,c,c, b,b,b, a,a,a)
-			else:
-				a,b,c,d = face[0]+ofs, face[1]+ofs, face[2]+ofs, face[3]+ofs
-				print >>file, "f %d/%d/%d %d/%d/%d %d/%d/%d %d/%d/%d" % (d,d,d, c,c,c, b,b,b, a,a,a)
-		ofs += len(mesh.positions)
-
-# Annotate materials with flags from data in .material files.
-
-def basename(str):
-	str = str.split('\\')[-1]
-	str = str.split('/')[-1]
-	str = str.split('.')[0]
-	str = str.lower().replace(' ', '_')
-	return str
-
-def make_material(mat, texture):
-	list = []
-	if 'twosided' in mat: list += ['twosided']
-	#if 'alphatest' in mat: list += ['alphatest']
-	if 'alphablend' in mat: list += ['alphablend']
-	#if 'alphagloss' in mat: list += ['alphagloss']
-	if 'unlit' in mat: list += ['unlit']
-	if 'clipu' in mat: list += ['clipu=%g' % mat['clipu']]
-	if 'clipv' in mat: list += ['clipv=%g' % mat['clipv']]
-	#if 'clipw' in mat: list += ['clipw=%g' % mat['clipw']]
-	#if 'cliph' in mat: list += ['cliph=%g' % mat['cliph']]
-	#if 'diffuse.file' in mat: list += [mat['diffuse.file']]
-	#elif 'specular.file' in mat: list += [mat['specular.file']]
-	#else: list += [texture]
-	list += [texture]
-	return list
-
-def load_material(file):
-	lib = {}
-	mat = None
-	tex = None
-	for line in file:
-		line = line.strip()
-		if line.startswith("material "):
-			name = line.split(' ', 1)[1]
-			name = name.lower().replace(' ', '_').replace('#', '_')
-			mat = {}
-			lib[name] = mat
-		elif line.startswith("texture 1"):
-			tex = "diffuse"
-		elif line.startswith("texture 2"):
-			tex = "specular"
-		elif line.startswith("texture "):
-			tex = "texture" + line.split(' ')[1]
-		elif '=' in line:
-			key, val = line.split('=', 1)
-			if key == 'twoSided' and val == 'true': mat['twosided'] = True
-			if key == 'bTwoSided' and val == 'true': mat['twosided'] = True
-			if key == 'bAlphaTest' and val == 'true': mat['alphatest'] = True
-			if key == 'bAlphaBlend' and val == 'true': mat['alphablend'] = True
-			if key == 'iShaderType' and val == '5': mat['alphagloss'] = True
-			if key == 'bUnlighted' and val == 'true': mat['unlit'] = True
-			if key == 'bitmap1FileName' and val: mat[tex+".file1"] = basename(val)
-			if key == 'bitmap2FileName' and val: mat[tex+".file2"] = basename(val)
-			if key == 'bitmap3FileName' and val: mat[tex+".file3"] = basename(val)
-			if key == 'bitmap4FileName' and val: mat[tex+".file4"] = basename(val)
-			if key == 'bitmap5FileName' and val: mat[tex+".file5"] = basename(val)
-			if key == 'bitmap6FileName' and val: mat[tex+".file6"] = basename(val)
-			if key == 'bitmap7FileName' and val: mat[tex+".file7"] = basename(val)
-			if key == 'bitmap8FileName' and val: mat[tex+".file8"] = basename(val)
-			if key == 'bitmap.filename' and val: mat[tex+".file"] = basename(val)
-			if key == 'bitmap.clipu' and float(val) != 0: mat['clipu'] = float(val)
-			if key == 'bitmap.clipv' and float(val) != 0: mat['clipv'] = float(val)
-			if key == 'bitmap.clipw' and float(val) != 1: mat['clipw'] = float(val)
-			if key == 'bitmap.cliph' and float(val) != 1: mat['cliph'] = float(val)
-	#print >>sys.stderr, lib
-	return lib
-
-def annotate_model(model, annots):
-	for mesh in model.meshes:
-		name = mesh.material[0]
-		texture = mesh.material[-1]
-		if name in annots:
-			mesh.material = make_material(annots[name], texture)
-		else:
-			name = name.replace("_1", "")
-			if name in annots:
-				mesh.material = make_material(annots[name], texture)
-
-# Create backfacing copies of twosided meshes.
-
-def backface_mesh(mesh):
-	print >>sys.stderr, "backface mesh:", mesh.name
+def doublesided_mesh(mesh):
+	print >>sys.stderr, "doublesided mesh:", mesh.name
 	mirror = Mesh(mesh.name + ".back")
 	mirror.material = mesh.material
 	mirror.positions = mesh.positions
@@ -303,21 +204,44 @@ def backface_mesh(mesh):
 			mirror.faces.append((d,c,b,a))
 	return mirror
 
-def backface_model(model):
+def singlesided_mesh(mesh):
+	print >>sys.stderr, "singlesided mesh:", mesh.name
+	mirror = Mesh(mesh.name + ".back")
+	mirror.material = mesh.material
+	mirror.positions = mesh.positions
+	mirror.normals = mesh.normals
+	mirror.texcoords = mesh.texcoords
+	mirror.colors = mesh.colors
+	mirror.blends = mesh.blends
+	mirror.custom = mesh.custom
+	mirror.faces = []
+	for face in mesh.faces:
+		if len(face) == 3:
+			a, b, c = face
+			mirror.faces.append((c,b,a))
+		else:
+			a, b, c, d = face
+			mirror.faces.append((d,c,b,a))
+	return mirror
+
+def doublesided_model(model):
 	extra = []
 	for mesh in model.meshes:
-		if 'twosided' in mesh.material:
-			mesh.material.remove('twosided')
-			extra.append(backface_mesh(mesh))
+		if 'doublesided' in mesh.material:
+			mesh.material.remove('doublesided')
+			extra.append(doublesided_mesh(mesh))
 	model.meshes += extra
 
-# Normal thief
+def singlesided_model(model):
+	extra = []
+	for mesh in model.meshes:
+		if 'singlesided' in mesh.material:
+			mesh.material.remove('singlesided')
+			extra.append(singlesided_mesh(mesh))
+	model.meshes += extra
 
-def normalize(x, y, z):
-	d = math.sqrt(x*x + y*y + z*z)
-	if d >= 0.001:
-		return x/d, y/d, z/d
-	return 0, 0, 1
+
+# Radial normals.
 
 def normal_thief_grass(mesh):
 	mesh.normals = [(0,0,1)] * len(mesh.normals)
@@ -337,30 +261,53 @@ def normal_thief_tree(mesh):
 		x, y, z = mesh.positions[i]
 		mesh.normals[i] = normalize(x-cx, y-cy, z-cz)
 
-def normal_thief_backface(mesh):
-	mirror = []
-	for face in mesh.faces:
-		if len(face) == 3:
-			a, b, c = face
-			mirror.append((c,b,a))
-		else:
-			a, b, c, d = face
-			mirror.append((d,c,b,a))
-	mesh.faces += mirror
+def calc_mesh_center(mesh):
+	cx, cy, cz = 0, 0, 0
+	for x,y,z in mesh.positions:
+		cx += x
+		cy += y
+		cz += z
+	cx /= len(mesh.positions)
+	cy /= len(mesh.positions)
+	cz /= len(mesh.positions)
+	return cx, cy, cz
 
-def normal_thief(model, remove_tags=False, backface=True):
+def calc_mesh_base(mesh):
+	cx, cy, cz = 0, 0, 1e10
+	for x,y,z in mesh.positions:
+		cx += x
+		cy += y
+		cz = min(cz, z)
+	cx /= len(mesh.positions)
+	cy /= len(mesh.positions)
+	return cx, cy, cz
+
+def calc_radial_mesh(mesh, centers):
+	print >>sys.stderr, "radial normals:", centers
+	cx, cy, cz = 0, 0, 0
+	for i, p in enumerate(mesh.positions):
+		x, y, z = p
+		nearest_pos, nearest_dist = (0, 0, 0), 1e10
+		for c in centers:
+			dx, dy, dz = x-c[0], y-c[1], z-c[2]
+			dist = dx*dx + dy*dy + dz*dz
+			if dist < nearest_dist:
+				nearest_dist = dist
+				cx, cy, cz = c
+		mesh.normals[i] = normalize(x-cx, y-cy, z-cz)
+
+def calc_radial_model(model):
 	for mesh in model.meshes:
-		if 'grass' in mesh.material:
-			normal_thief_grass(mesh)
-			if remove_tags: mesh.material.remove('n:grass')
-		if 'tree' in mesh.material:
-			normal_thief_tree(mesh)
-			if remove_tags: mesh.material.remove('n:tree')
-		if backface and 'twosided' in mesh.material:
-			normal_thief_backface(mesh)
-			mesh.material.remove('twosided')
+		centers = []
+		print >>sys.stderr, "radial mats", mesh.material
+		for tag in mesh.material:
+			if tag == "radial=center": centers.append(calc_mesh_center(mesh))
+			elif tag == "radial=base": centers.append(calc_mesh_base(mesh))
+			elif tag.startswith("radial="): centers.append(tuple([float(x) for x in tag[7:].split(",")]))
+		if len(centers) > 0:
+			calc_radial_mesh(mesh, centers)
 
-# Apply global scale to all mesh coords
+# Transform model.
 
 def scale_model(model, s):
 	for mesh in model.meshes:
@@ -379,8 +326,6 @@ def scale_model(model, s):
 				pose[1] *= s
 				pose[2] *= s
 				frame[i] = tuple(pose)
-
-# Translate model coords
 
 def translate_model(model, tx, ty, tz):
 	if len(model.bindpose) > 0:
@@ -404,58 +349,17 @@ def drop_to_ground(model):
 				min_z = z
 	translate_model(model, 0, 0, -min_z)
 
-# Flip X to Y (for weapon models to realign with blender bones)
-
-def x_to_y(v):
-	x, y, z = v
-	return -y, x, z
-
 def model_x_to_y(model):
+	# TODO: bones
 	for mesh in model.meshes:
 		mesh.positions = [x_to_y(v) for v in mesh.positions]
 		mesh.normals = [x_to_y(v) for v in mesh.normals]
 
-def y_to_z(v):
-	x, y, z = v
-	return x, -z, y
-
 def model_y_to_z(model):
+	# TODO: bones
 	for mesh in model.meshes:
 		mesh.positions = [y_to_z(v) for v in mesh.positions]
 		mesh.normals = [y_to_z(v) for v in mesh.normals]
-
-# Fix UV coords that have a clip region set. Ugh.
-# We can offset the origin, but then the repeat/wrapping fails.
-# We can offset the origin, and wrap the U/V coords, but then it wraps
-# the wrong way.
-# We can remap the uv space, but then we have to extract the cropped region
-# of the texture as a separate texture.
-
-def clipuv_mesh(mesh, uofs, vofs, w, h):
-	print >>sys.stderr, "clipuv", mesh.name, uofs, vofs, w, h
-	newtc = []
-	for u,v in mesh.texcoords:
-		#if u > w: u -= w
-		#if v > h: v -= h
-		u += uofs;
-		v += vofs;
-		#u = u / w
-		#v = v / h
-		newtc.append((u, v))
-	mesh.texcoords = newtc
-
-def clipuv_model(model):
-	for mesh in model.meshes:
-		u = v = 0
-		w = h = 1
-		ua = va = wa = ha = None
-		for a in mesh.material:
-			if a.startswith('clipu='): u = float(a.split('=')[1]); ua = a
-			if a.startswith('clipv='): v = float(a.split('=')[1]); va = a
-			if a.startswith('clipw='): w = float(a.split('=')[1]); wa = a
-			if a.startswith('cliph='): h = float(a.split('=')[1]); ha = a
-		if u != 0 or v != 0 or w != 1 or h != 1:
-			clipuv_mesh(mesh, u, v, w, h)
 
 # Merge meshes with the same material.
 
@@ -646,8 +550,3 @@ def kill_anim_scaling(model):
 		for frame in anim.frames:
 			for i in range(len(frame)):
 				frame[i] = frame[i][0:7]
-
-if __name__ == "__main__":
-	for filename in sys.argv[1:]:
-		load_model(open(filename)).save(sys.stdout)
-
